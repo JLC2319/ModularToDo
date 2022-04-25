a0af015d855ac8f3cc8f4eccde5382774d280144
JLC2319 <jamescraig2319@gmail.com>
2022-04-25 06:43:28 -0500

    planning

--- toDoEngine.js
+++ toDoEngine.js
@@ -1,1 +1,22 @@
 
+
+const fs = require('fs')
+{
+    class ModularToDo{
+        constructor(path = 'myToDoData/toDoList.json'){
+            this.examples = {
+                groceryTrip: {},
+                sundayFunday: {},
+            }//Todo: finish examples
+
+            this.toDoList=JSON.parse(fs.readFileSync(path))
+            //Todo: if no file, then make new folder&file
+            //Todo: Separate JSON for completeds, only accesses recents to save memory usage
+            
+                
+        }
+        //Todo: make to do function
+
+
+    }
+}
